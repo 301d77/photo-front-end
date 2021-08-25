48b0071fdaed6f3f0925acba6fdc9c1425c3dc0d
JB-Tellez <jb.tellez@gmail.com>
2021-08-24 23:48:03 -0700

    adds initial files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Form from 'react-bootstrap/Form';
+import axios from 'axios';
+import Photos from './photos';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchQuery: '',
+      photos: [],
+    }
+  }
+
+  updateSearchQuery = (e) => this.setState({ searchQuery: e.target.value });
+
+  getPhotos = async (e) => {
+    e.preventDefault();
+
+    try {
+      const API = process.env.REACT_APP_API_URL;
+      const photos = await axios.get(`${API}/photo`, { params: { query: this.state.searchQuery } });
+      this.setState({ photos: photos.data });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <Form onSubmit={this.getPhotos}>
+          <Form.Group controlId="searchQuery">
+            <Form.Label>Find Photos About...</Form.Label>
+            <Form.Control onChange={this.updateSearchQuery} type="text" placeholder="Enter a search term" />
+          </Form.Group>
+        </Form>
+
+        {this.state.photos.length > 0 &&
+          <Photos
+            photos={this.state.photos}
+            searchQuery={this.state.searchQuery}
+          />
+        }
+      </>
+    )
+  }
 }
 
 export default App;
